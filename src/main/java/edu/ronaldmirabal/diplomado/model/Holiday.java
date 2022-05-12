72dab59804a016d04fa878f7ec64f469d0925877
ronaldmirabal <ronaldmirabal@outlook.es>
2022-05-12 11:28:34 -0400

    Colocando tarea homework week2

---
+++ src/main/java/edu/ronaldmirabal/diplomado/model/Holiday.java
@@ -0,0 +1,17 @@
+package edu.ronaldmirabal.diplomado.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Holiday {
+    private String name;
+    private LocalDate date;
+}
---
+++ src/main/java/edu/ronaldmirabal/diplomado/week2/HomeWork.java
@@ -0,0 +1,29 @@
+package edu.ronaldmirabal.diplomado.week2;
+
+import edu.ronaldmirabal.diplomado.model.Holiday;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Crear una mejora de cualquiera de los algoritmos optimizados para pasarle los días laborales (no asumir, que llegue como parámetro)
+ *
+ * @author Ronald Mirabal on 3/27/2022.
+ * @implNote Debe tener sus test
+ */
+public class HomeWork {
+    /**
+     * Hacer una implementación propia basado en los ejemplos explicados en clase.
+     *
+     * @param laborDays Los dias que son laborables, es decir, podemos trabajar de lunes a viernes, de lunes a sábado, o la semana entera, o solo 3 dias a la semana
+     * @param holidays  Todos los dias feriados que serán relevantes para el cálculo, tomar en cuenta diciembre para esto, puede que necesites los feriados del año siguiente
+     * @param startDate Fecha de inicio para calcular las vacaciones, esta fecha es incluyente, es decir, desde aquí se inicia la vacación
+     * @param vacation  La cantidad de día a vacacionar
+     * @return La fecha que debe retornar el trabajador
+     * @implNote El último día de vacación también cuenta, es decir, que regresa el siguiente dia laboral
+     */
+    public LocalDate calculateDueDate(List<DayOfWeek> laborDays, List<Holiday> holidays, LocalDate startDate, int vacation) {
+        return null;
+    }
+}
